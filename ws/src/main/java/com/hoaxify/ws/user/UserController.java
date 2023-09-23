bbbf5a171c8bb4e598bc2588de68b378bada470d
cgezer <celalgezer@gmail.com>
2023-09-23 22:47:00 +0300

    UniqueUsernameValidator

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -2,7 +2,6 @@ package com.hoaxify.ws.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
